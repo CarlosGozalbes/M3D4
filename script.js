b433a9b09a8739027c847fba9cdd2d1ddf23566d
Carlos Gozalbes <84912506+CarlosGozalbes@users.noreply.github.com>
2021-12-07 04:22:34 +0100

    Bookstore

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-const loadBooks = () => {
-  return fetch("https://striveschool-api.herokuapp.com/books", {})
-    .then((res) => res.json())
+const shoppingCart = document.querySelector("#cart");
 
-    .then((books) => {
+let books = [];
+
+let shoppingCartList = [];
+
+let filteredBooks = [];
+
+function loadBooks() {
+  fetch("https://striveschool-api.herokuapp.com/books")
+    .then((r) => r.json())
+    .then((_books) => {
+      books = _books;
       console.log(books);
-
+      displayBooks();
+    })
+    .catch((err) => console.error(err.message));
+}
+function displayBooks(_books = books) {
       books.forEach((book) => {
         const row = document.querySelector(".row");
         const col = document.createElement("div");
-
+        col.classList.add("mb-3")
         col.classList.add("col-md-3");
         col.innerHTML = `
-                    <div class="card h-100 mb-4 shadow-sm">
+                    <div class="parent card h-100 mb-4 shadow-sm">
                     <img src="${book.img}" alt="..." class="card-img-top img-fluid" style="width: 100%;
                     height: 15vw;
                     object-fit: cover;">
@@ -22,10 +34,10 @@ const loadBooks = () => {
                     </p>
                     <div id="card-buttons" class="d-flex justify-content-between align-items-bottom">
                         <div class="btn-group">
-                        <button type="button" class="shop btn btn-sm btn-outline-secondary">
+                        <button type="button" class="shop btn btn-sm btn-outline-secondary" onclick="addToCart('${String(book.asin)}', this)">
                             Add to card
                         </button>
-                        <button type="button delete" class="btn btn-sm btn-outline-secondary">
+                        <button type="button" class="delete btn btn-sm btn-outline-secondary" onclick="this.closest('.parent').remove()">
                             Skip
                         </button>
                         </div>                    
@@ -35,7 +47,8 @@ const loadBooks = () => {
                     `;
         row.appendChild(col);
       });
-    })
+}
+   
 
     /* const row = document.querySelector(".row")
 
@@ -59,22 +72,44 @@ const loadBooks = () => {
                         row.appendChild(col)
                     }
  */
-    .catch((error) => console.log(error));
-};
+
+
+(function(){
+  const cartInfo = document.getElementById('cart-info');
+  const cart = document.getElementById('cart');
+  cartInfo.addEventListener('click', function(){
+    cart.classList.toggle('show-cart')
+    console.log('clickedcart') 
+})
+})();
+
+
+
+/* 
+const showcart = () => {
+  const cartInfo = document.getElementById("cart-info")
+  const cart = document.getElementById("cart")
+  cartInfo.addEventListener("click", function(){
+    cart.classList.toogle("show-cart")
+    console.log("clickedcart")
+  })
+}
+ */
+
 
 window.onload = () => {
-  loadBooks().then(() => {
+  loadBooks()/* .then(() => {
     const buttons =
       document.querySelectorAll(
         ".shop"
-      ); /* .addEventListener("click", (event)=> {buyItem(event.target)}); */
-    for (let i = 0; i < buttons.length; i++) {
+      ); */ /* .addEventListener("click", (event)=> {buyItem(event.target)}); */
+    /* for (let i = 0; i < buttons.length; i++) {
       const button = buttons[i];
       button.addEventListener("click", (event) => {
         buyItem(event.target);
       });
     }
-  });
+  }); */
 };
 /* window.onload = () => {
     document.querySelector(".search").addEventListener("click", loadBooks) */
@@ -99,13 +134,13 @@ function buyItem() {
     card.classList.add('on-the-cart');
 } */
 
-const buyItem = (node) => {
+/* const buyItem = (node) => { */
   /* node.parentElement.parentElement.classList.add("on-the-cart");
   const li = document.createElement("li"); */
-  console.log("fdasfsd");
+  /* console.log("fdasfsd"); */
   /* li.innerHTML = `${book.title}`; */
   /* document.getElementById("list").appendChild(li); */
-};
+/* }; */
 
 /* const removeCard = () => {
   node.parentElement.parentElement.classlist.add("removed");
@@ -128,4 +163,83 @@ document
 
     console.log(filteredBooks);
     displayBooks(filteredBooks);
+  }
+
+
+  function deleteItem(asin) {
+    const index = shoppingCartList.findIndex((book) => book.asin === asin);
+
+    if (index !== -1) {
+      shoppingCartList.splice(index, 1);
+    }
+
+    refreshShoppingCart();
+  } 
+ 
+ cart
+ 
+  /* function search(query) {
+    if (query.length < 3) {
+      filteredBooks = books;
+      displayBooks();
+      return;
+    }
+
+    filteredBooks = books.filter((book) =>
+      book.title.toLowerCase().includes(query.toLowerCase())
+    );
+
+    console.log(filteredBooks);
+    displayBooks(filteredBooks);
+  } */
+
+
+  (function(){
+    const cartBtn = document.querySelectorAll('.shop')
+    cartBtn.forEach(function(btn){
+      btn.addEventListener('click',function(event){
+
+        if(event.target.parentElement.classList.contains('.shop')){
+          console.log(event.target.parentElement.parentElement)
+        }
+
+      })
+    })
+  })
+
+
+  function addToCart(asin, element) {
+    console.log(asin);
+    // const book = books.filter( book => book.asin === asin)[0]
+    const book = books.find((book) => book.asin == asin);
+    shoppingCartList.push(book);
+    console.log(shoppingCartList);
+
+    refreshShoppingCart();
+
+    element.closest(".parent").classList.add("selected");
+  }
+
+  function refreshShoppingCart() {
+    shoppingCart.innerHTML = "";
+
+    shoppingCartList.forEach((book) => {
+      shoppingCart.innerHTML += `  
+
+        <div class="cart-item d-flex justify-content-between text-capitalize my-3">
+              <img src="${book.img}" class="card-img-top img-fluid pr-1" style="width: 40px;
+              
+              object-fit: cover;" id="item-img" alt="">
+              <div class="item-text">
+  
+                <p id="cart-item-title" class="font-weight-bold mb-0">${book.title}</p>
+                <span>€</span>
+                <span id="cart-item-price" class="cart-item-price" class="mb-0">${book.price}</span>
+              </div>
+              <a href="#" id='cart-item-remove' class="cart-item-remove d-flex align-items-center" onclick="deleteItem('${String(book.asin)}')">
+                <i class="bi bi-trash aling-center"></i>
+              </a>
+            </div>
+      `;
+    });
   }
